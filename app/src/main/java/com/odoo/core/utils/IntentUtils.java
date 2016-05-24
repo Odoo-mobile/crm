fd03aab53e86ebb1b4d6c3e48b78cefe3c5212bc
asa-odoo <asa@odoo.com>
2016-05-24 14:54:30 +0530

    [ADD] add permission check runtime for calling

--- app/src/main/java/com/odoo/core/utils/IntentUtils.java
+++ app/src/main/java/com/odoo/core/utils/IntentUtils.java
@@ -1,30 +1,38 @@
 /**
  * Odoo, Open Source Management Solution
  * Copyright (C) 2012-today Odoo SA (<http:www.odoo.com>)
- *
+ * <p/>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details
- *
+ * <p/>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http:www.gnu.org/licenses/>
- *
+ * <p/>
  * Created on 18/12/14 11:31 AM
  */
 package com.odoo.core.utils;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+import static android.support.v4.app.ActivityCompat.requestPermissions;
 
 public class IntentUtils {
+
+    private static final int REQUEST_CODE_ASK_PERMISSIONS_CALL_CONTACT = 1;
 
     public static void openURLInBrowser(Context context, String url) {
         if (!url.equals("false") && !url.equals("")) {
@@ -65,7 +73,11 @@ public class IntentUtils {
         if (!number.equals("false") && !number.equals("")) {
             Intent intent = new Intent(Intent.ACTION_CALL);
             intent.setData(Uri.parse("tel:" + number));
-            context.startActivity(intent);
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions((Activity) context, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE_ASK_PERMISSIONS_CALL_CONTACT);
+            } else {
+                context.startActivity(intent);
+            }
         }
     }
 
