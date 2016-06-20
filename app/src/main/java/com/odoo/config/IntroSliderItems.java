5b01aea1c8a09f5493d30ccb5f4d33336b339494
asa-odoo <asa@odoo.com>
2016-06-20 17:44:16 +0530

    [REF] refactor application launch activity to appIntro

--- app/src/main/java/com/odoo/config/IntroSliderItems.java
+++ app/src/main/java/com/odoo/config/IntroSliderItems.java
@@ -1,39 +1,38 @@
 /**
  * Odoo, Open Source Management Solution
  * Copyright (C) 2012-today Odoo SA (<http:www.odoo.com>)
- *
+ * <p/>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details
- *
+ * <p/>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http:www.gnu.org/licenses/>
- *
+ * <p/>
  * Created on 13/2/15 4:16 PM
  */
 package com.odoo.config;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.odoo.core.utils.OControls;
 import com.odoo.R;
+import com.odoo.core.utils.OControls;
 import com.odoo.widgets.slider.SliderItem;
 import com.odoo.widgets.slider.SliderPagerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class IntroSliderItems implements SliderPagerAdapter.SliderBuilderListener, View.OnClickListener {
+public class IntroSliderItems implements SliderPagerAdapter.SliderBuilderListener {
     public static final String TAG = IntroSliderItems.class.getSimpleName();
     private Context mContext;
 
@@ -74,22 +73,6 @@ public class IntroSliderItems implements SliderPagerAdapter.SliderBuilderListene
         OControls.setImage(view, R.id.slider_image, item.getImagePath());
         OControls.setText(view, R.id.sub_title, item.getExtras().get("sub_title").toString());
         OControls.setText(view, R.id.description, item.getContent());
-        if (item.getImagePath() == R.drawable.odoo_shaded) {
-            OControls.setGone(view, R.id.description);
-            OControls.setVisible(view, R.id.btnSliderFinish);
-            OControls.setText(view, R.id.btnSliderFinish, "GOT IT, LET'S GO!");
-            view.findViewById(R.id.btnSliderFinish).setOnClickListener(this);
-        } else {
-            OControls.setVisible(view, R.id.description);
-            OControls.setGone(view, R.id.btnSliderFinish);
-        }
         return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btnSliderFinish) {
-            ((Activity) mContext).finish();
-        }
     }
 }
