1f16a7e3d3051efeb0ae9a31348efecd29ebd301
Dharmang Soni <dpr@odoo.com>
2016-06-22 12:42:20 +0530

    [IMP] Imprved UI for setup activity and Intro slider

--- app/src/main/java/com/odoo/SetupActivity.java
+++ app/src/main/java/com/odoo/SetupActivity.java
@@ -3,29 +3,33 @@ package com.odoo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.odoo.core.support.OUser;
 import com.odoo.core.utils.BitmapUtils;
+import com.odoo.core.utils.OControls;
 
 public class SetupActivity extends AppCompatActivity {
 
-    private ImageView userAvatar;
+    private OUser mUser;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup);
+        setContentView(R.layout.base_activity_setup);
+        mUser = OUser.current(this);
         init();
-        OUser currentUser = OUser.current(this);
-        if (!currentUser.getAvatar().equals("false")) {
-            Bitmap bitmap = BitmapUtils.getBitmapImage(this, currentUser.getAvatar());
-            if (bitmap != null)
-                userAvatar.setImageBitmap(bitmap);
-        }
     }
 
     private void init() {
-        userAvatar = (ImageView) findViewById(R.id.user_avatar);
+        progressBar = (ProgressBar) findViewById(R.id.setupProgress);
+        OControls.setText(findViewById(android.R.id.content), R.id.txvUsername,
+                String.format("Hello %s", mUser.getName()));
+        if (!mUser.getAvatar().equals("false")) {
+            Bitmap bitmap = BitmapUtils.getBitmapImage(this, mUser.getAvatar());
+            if (bitmap != null)
+                OControls.setImage(findViewById(android.R.id.content), R.id.user_avatar, bitmap);
+        }
     }
 }
